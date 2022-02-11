633a9e86dada674734a5220f029dc338fda30a41
Tosin Omotayo <oluwaetosin@gmail.com>
2022-02-11 05:41:03 +0100

    added database setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,16 @@
 import express from 'express'
 
+
+import makeTables from './database/initidb';
+
 const app = express();
+
+
 const port = 3000;
+
+app.listen(port, ()=>{
+
+    makeTables();
+});
+
 
