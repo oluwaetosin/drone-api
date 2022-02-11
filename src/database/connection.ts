6956aeb3a64bacfc8f5f5e70927c411d58b0fe3a
Tosin Omotayo <oluwaetosin@gmail.com>
2022-02-11 20:02:01 +0100

    updated database name

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,11 +2,12 @@ import sqlite3 from 'sqlite3';
 
 const sqlite  =  sqlite3.verbose();
 
-const db = new sqlite.Database(':memory:', (err)=>{
+const db = new sqlite.Database('memory_db', (err)=>{
     if(err){
       return  console.log(err);
     }
     console.log("Connected to memeory database");
-})
+});
+
 
 export default db;
